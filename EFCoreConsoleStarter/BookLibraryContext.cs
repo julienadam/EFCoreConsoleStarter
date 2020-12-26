63145604e84899c283157edab17746f6599c2874
Julien Adam <julien.adam@arcodia.fr>
2020-12-26 11:02:19 +0100

    Added context factories to design time configuration

--- EFCoreConsoleStarter/BookLibraryContext.cs
+++ EFCoreConsoleStarter/BookLibraryContext.cs
@@ -3,13 +3,13 @@ using Microsoft.EntityFrameworkCore;
 
 namespace EFCoreConsoleStarter
 {
+    // This class is used by the dotnet ef tools to create db contexts when the program is not running
+    public class BookLibraryDesignTimeDbContextFactory : DesignTimeDbContextFactory<BookLibraryContext> { }
+    
     public class BookLibraryContext : DbContext
     {
-        public BookLibraryContext(DbContextOptions options) : base(options)
-        {
-            
-        }
-        
+        public BookLibraryContext(DbContextOptions options) : base(options) { }
+
         public DbSet<Book> Books { get; set; }
     }
 }
---
+++ EFCoreConsoleStarter/BookLibraryContextFactory.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.IO;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+using Microsoft.Extensions.Configuration;
+
+namespace EFCoreConsoleStarter
+{
+    public class DesignTimeDbContextFactory<T> : IDesignTimeDbContextFactory<T> where T: DbContext
+    {
+        public T CreateDbContext(string[] args)
+        {
+            var configuration = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetParent(AppContext.BaseDirectory).FullName)
+                .AddJsonFile("appsettings.json", false)
+                .Build();
+
+            var optionsBuilder = new DbContextOptionsBuilder<BookLibraryContext>();
+            optionsBuilder.UseSqlServer(configuration.GetConnectionString("DataConnection"));
+
+            return (T) Activator.CreateInstance(typeof(T), optionsBuilder.Options);
+        }
+    }
+
+}
