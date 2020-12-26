acb89202c978882426bcc78f97f630ef7aa0385b
Julien Adam <julien.adam@arcodia.fr>
2020-12-26 10:09:24 +0100

    Added init code for settings and logging

--- EFCoreConsoleStarter/Program.cs
+++ EFCoreConsoleStarter/Program.cs
@@ -1,12 +1,45 @@
 ﻿using System;
+using System.IO;
+using Microsoft.Extensions.Configuration;
+using Serilog;
 
 namespace EFCoreConsoleStarter
 {
-    class Program
+    internal class Program
     {
-        static void Main(string[] args)
+        private static int ConfiguredMain(IConfigurationRoot configuration, string[] args)
         {
-            Console.WriteLine("Hello World!");
+            // Votre code ici
+            return 1;
+        }
+
+        private static int Main(string[] args)
+        {
+            // Initialize Serilog logger
+            Log.Logger = new LoggerConfiguration()
+                 .WriteTo.Console(Serilog.Events.LogEventLevel.Debug)
+                 .MinimumLevel.Debug()
+                 .Enrich.FromLogContext()
+                 .CreateLogger();
+
+            var configuration = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetParent(AppContext.BaseDirectory).FullName)
+                .AddJsonFile("appsettings.json", false)
+                .Build();
+            
+            try
+            {
+                return ConfiguredMain(configuration, args);
+            }
+            catch (Exception ex)
+            {
+                Log.Fatal(ex, "Unhandled exception in Main.");
+                return 1;
+            }
+            finally
+            {
+                Log.CloseAndFlush();
+            }
         }
     }
 }
