5cfae92e74180d72e66404a74352d043e6dd0bfe
Julien Adam <julien.adam@arcodia.fr>
2020-12-26 09:49:50 +0100

    Initial commit with empty project

---
+++ EFCoreConsoleStarter/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace EFCoreConsoleStarter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
