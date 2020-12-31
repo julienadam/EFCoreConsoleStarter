308fa73b3e5247d176a720c2641b633b1c7166d3
Julien Adam <julien.adam@arcodia.fr>
2020-12-31 14:02:36 +0100

    Commented out sample code

--- EFCoreConsoleStarter/App.cs
+++ EFCoreConsoleStarter/App.cs
@@ -17,45 +17,48 @@ namespace EFCoreConsoleStarter
         
         public int Run(string[] args)
         {
-            // Adding an entity
-            using (var context = contextFactory.CreateDbContext())
-            {
-                // Adding a book
-                context.Add(new Book
-                {
-                    Authors = "J.R.R. Tolkien",
-                    Isbn = "9780261102385",
-                    Title = "The Lord of the Rings",
-                    PublicationDate = new DateTime(1954, 01, 01),
-                    Summary = "A hobbit and his allies go to Mordor to throw a magic ring in a volcano."
-                });
-                context.SaveChanges();
-            }
+            // Your code goes here, check out the samples below
+            
+            
+            //// Adding an entity
+            //using (var context = contextFactory.CreateDbContext())
+            //{
+            //    // Adding a book
+            //    context.Add(new Book
+            //    {
+            //        Authors = "J.R.R. Tolkien",
+            //        Isbn = "9780261102385",
+            //        Title = "The Lord of the Rings",
+            //        PublicationDate = new DateTime(1954, 01, 01),
+            //        Summary = "A hobbit and his allies go to Mordor to throw a magic ring in a volcano."
+            //    });
+            //    context.SaveChanges();
+            //}
 
-            // Modifying an entity
-            using (var context = contextFactory.CreateDbContext())
-            {
-                var lotr = context.Books.First(b => b.Isbn == "9780261102385");
-                lotr.Summary = "Sauron, an evil overlord, tries to conquer the world but a band of unlikely heroes gathers to thwart his plans.";
-                context.SaveChanges();
-            }
+            //// Modifying an entity
+            //using (var context = contextFactory.CreateDbContext())
+            //{
+            //    var lotr = context.Books.First(b => b.Isbn == "9780261102385");
+            //    lotr.Summary = "Sauron, an evil overlord, tries to conquer the world but a band of unlikely heroes gathers to thwart his plans.";
+            //    context.SaveChanges();
+            //}
 
-            using (var context = contextFactory.CreateDbContext())
-            {
-                foreach (var book in context.Books)
-                {
-                    Console.WriteLine($"{book.Isbn} : {book.Title}");
-                    Console.WriteLine($"{book.Summary}");
+            //using (var context = contextFactory.CreateDbContext())
+            //{
+            //    foreach (var book in context.Books)
+            //    {
+            //        Console.WriteLine($"{book.Isbn} : {book.Title}");
+            //        Console.WriteLine($"{book.Summary}");
 
-                    if (book.Isbn == "9780261102385")
-                    {
-                        // Deleting an entity
-                        context.Remove(book);
-                    }
-                }
+            //        if (book.Isbn == "9780261102385")
+            //        {
+            //            // Deleting an entity
+            //            context.Remove(book);
+            //        }
+            //    }
                 
-                context.SaveChanges();
-            }
+            //    context.SaveChanges();
+            //}
 
             return 1;
         }
